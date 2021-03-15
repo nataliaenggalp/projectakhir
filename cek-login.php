2e7a7affbaeae8c52c3a30271cd530012ec2de85
nataliaenggalp <60637039+nataliaenggalp@users.noreply.github.com>
2021-03-15 14:48:15 +0700

    Delete cek-login.php

--- cek-login.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-
-
-if ((!isset($_SESSION['usernamecalonpekerja']) || empty($_SESSION['usernamecalonpekerja'])) or (!isset($_SESSION['usernameperusahaan']) || empty($_SESSION['usernameperusahaan'])) ) {
-	//redirect ke halaman login
-	
-}
-?>
